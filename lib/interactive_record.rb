d7f23d2261aec8c105fe367976ca67254b45964a
csdj92 <csdj92@gmail.com>
2020-10-06 13:42:59 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,14 +43,14 @@ class InteractiveRecord
     end
     values.join(", ")
   end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
 def self.find_by_name(name)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
   DB[:conn].execute(sql, name)
 end
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+
 
 end
